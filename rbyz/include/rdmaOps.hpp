f39d8f159231d381fe6ee8cbcd3768f25e7bb4c2
Wousta <luisbmi55@gmail.com>
2025-06-07 11:31:53 +0200

    polling of cq implemented for no_sync rdma operations

--- rbyz/include/rdmaOps.hpp
+++ rbyz/include/rdmaOps.hpp
@@ -14,8 +14,6 @@ using ltncyVec = std::vector<std::pair<int, std::chrono::nanoseconds::rep>>;
 class RdmaOps {
   private:
   std::vector<RcConn> conns;
-  std::vector<comm_info> conn_data;
-  std::vector<unsigned int> posted_wqes;
   std::shared_ptr<ltncyVec> latency;
   NetFlags netflags_sync;
   NetFlags netflags_no_sync;
@@ -24,15 +22,15 @@ class RdmaOps {
   RdmaOps(std::vector<RcConn>& conns);
   ~RdmaOps();
 
-  int exec_rdma_read(uint32_t size, uint32_t loc_info_idx, uint32_t rem_info_idx, int conn_data_idx = 0);
-  int exec_rdma_read(uint32_t size, uint32_t same_idx, int conn_data_idx = 0);
+  int exec_rdma_read(uint32_t size, uint32_t loc_info_idx, uint32_t rem_info_idx, int conn_idx = 0);
+  int exec_rdma_read(uint32_t size, uint32_t same_idx, int conn_idx = 0);
   
-  int exec_rdma_write(uint32_t size, uint32_t loc_info_idx, uint32_t rem_info_idx, int conn_data_idx = 0);
-  int exec_rdma_write(uint32_t size, uint32_t same_idx, int conn_data_idx = 0);
-  int exec_rdma_write(uint32_t size, LocalInfo &local_info, RemoteInfo &remote_info, int conn_data_idx = 0, bool is_sync = false);
+  int exec_rdma_write(uint32_t size, uint32_t loc_info_idx, uint32_t rem_info_idx, int conn_idx = 0);
+  int exec_rdma_write(uint32_t size, uint32_t same_idx, int conn_idx = 0);
+  int exec_rdma_write(uint32_t size, LocalInfo &local_info, RemoteInfo &remote_info, int conn_idx = 0, bool is_sync = false);
   
-  int exec_rdma_CAS(uint32_t size, uint32_t loc_info_idx, uint32_t rem_info_idx, uint64_t compare_add, uint64_t swap, int conn_data_idx = 0);
-  int exec_rdma_CAS(uint32_t size, uint32_t same_idx, uint64_t compare_add, uint64_t swap, int conn_data_idx = 0);
+  int exec_rdma_CAS(uint32_t size, uint32_t loc_info_idx, uint32_t rem_info_idx, uint64_t compare_add, uint64_t swap, int conn_idx = 0);
+  int exec_rdma_CAS(uint32_t size, uint32_t same_idx, uint64_t compare_add, uint64_t swap, int conn_idx = 0);
 
-  void read_mnist_update(std::vector<torch::Tensor> &update, float *local_w, int same_idx, int conn_data_idx = 0);
+  void read_mnist_update(std::vector<torch::Tensor> &update, float *local_w, int same_idx, int conn_idx = 0);
 };
--- rbyz/src/rdmaOps.cpp
+++ rbyz/src/rdmaOps.cpp
@@ -4,76 +4,66 @@
 #include "util.hpp"
 #include "logger.hpp"
 
-RdmaOps::RdmaOps(std::vector<RcConn>& conns) : 
-  conns(conns),
-  posted_wqes(conns.size(), 0),
-  latency(std::make_shared<ltncyVec>()) {
-    
+RdmaOps::RdmaOps(std::vector<RcConn>& conns) : conns(conns), latency(std::make_shared<ltncyVec>()) {    
   latency->reserve(10);
-
   netflags_no_sync.is_sync = false;
-
-  for (auto &conn : conns) {
-    conn_data.push_back(conn.getConnData()); 
-  }
-
 }
 
 RdmaOps::~RdmaOps() {
   // Do nothing
 }
 
-int RdmaOps::exec_rdma_read(uint32_t size, uint32_t loc_info_idx, uint32_t rem_info_idx, int conn_data_idx) {
+int RdmaOps::exec_rdma_read(uint32_t size, uint32_t loc_info_idx, uint32_t rem_info_idx, int conn_idx) {
   LocalInfo local_info;
   local_info.indices.push_back(loc_info_idx);
   RemoteInfo remote_info;
   remote_info.indx = rem_info_idx;
-  return norm::read(this->conn_data[conn_data_idx], {size}, {local_info}, netflags_sync, remote_info, this->latency, posted_wqes[conn_data_idx]);
+  return norm::read(conns[conn_idx], {size}, {local_info}, netflags_sync, remote_info, this->latency);
 }
 
-int RdmaOps::exec_rdma_write(uint32_t size, uint32_t loc_info_idx, uint32_t rem_info_idx, int conn_data_idx) {
+int RdmaOps::exec_rdma_write(uint32_t size, uint32_t loc_info_idx, uint32_t rem_info_idx, int conn_idx) {
   LocalInfo local_info;
   local_info.indices.push_back(loc_info_idx);
   RemoteInfo remote_info;
   remote_info.indx = rem_info_idx;
-  return norm::write(this->conn_data[conn_data_idx], {size}, {local_info}, netflags_sync, remote_info, this->latency, posted_wqes[conn_data_idx]);
+  return norm::write(conns[conn_idx], {size}, {local_info}, netflags_sync, remote_info, this->latency);
 }
 
-int RdmaOps::exec_rdma_write(uint32_t size, LocalInfo &local_info, RemoteInfo &remote_info, int conn_data_idx, bool is_sync) {
+int RdmaOps::exec_rdma_write(uint32_t size, LocalInfo &local_info, RemoteInfo &remote_info, int conn_idx, bool is_sync) {
   if (is_sync) {
-    return norm::write(this->conn_data[conn_data_idx], {size}, {local_info}, netflags_sync, remote_info, this->latency, posted_wqes[conn_data_idx]);
+    return norm::write(conns[conn_idx], {size}, {local_info}, netflags_sync, remote_info, this->latency);
   } else {
-    return norm::write(this->conn_data[conn_data_idx], {size}, {local_info}, netflags_no_sync, remote_info, this->latency, posted_wqes[conn_data_idx]);
+    return norm::write(conns[conn_idx], {size}, {local_info}, netflags_no_sync, remote_info, this->latency);
   }
 }
 
-int RdmaOps::exec_rdma_CAS(uint32_t size, uint32_t loc_info_idx, uint32_t rem_info_idx, uint64_t compare_add, uint64_t swap, int conn_data_idx) {
+int RdmaOps::exec_rdma_CAS(uint32_t size, uint32_t loc_info_idx, uint32_t rem_info_idx, uint64_t compare_add, uint64_t swap, int conn_idx) {
   LocalInfo local_info;
   local_info.indices.push_back(loc_info_idx);
   local_info.compare_add = compare_add;
   local_info.swap = swap;
   RemoteInfo remote_info;
   remote_info.indx = rem_info_idx;
-  return norm::CAS(this->conn_data[conn_data_idx], {size}, {local_info}, netflags_sync, remote_info, this->latency, posted_wqes[conn_data_idx]);
+  return norm::CAS(conns[conn_idx], {size}, {local_info}, netflags_sync, remote_info, this->latency);
 }
 
-int RdmaOps::exec_rdma_read(uint32_t size, uint32_t same_idx, int conn_data_idx) {
-  return exec_rdma_read(size, same_idx, same_idx, conn_data_idx);
+int RdmaOps::exec_rdma_read(uint32_t size, uint32_t same_idx, int conn_idx) {
+  return exec_rdma_read(size, same_idx, same_idx, conn_idx);
 }
 
-int RdmaOps::exec_rdma_write(uint32_t size, uint32_t same_idx, int conn_data_idx) {
-  return exec_rdma_write(size, same_idx, same_idx, conn_data_idx);
+int RdmaOps::exec_rdma_write(uint32_t size, uint32_t same_idx, int conn_idx) {
+  return exec_rdma_write(size, same_idx, same_idx, conn_idx);
 }
 
-int RdmaOps::exec_rdma_CAS(uint32_t size, uint32_t same_idx, uint64_t compare_add, uint64_t swap, int conn_data_idx) {
-  return exec_rdma_CAS(size, same_idx, same_idx, compare_add, swap, conn_data_idx);
+int RdmaOps::exec_rdma_CAS(uint32_t size, uint32_t same_idx, uint64_t compare_add, uint64_t swap, int conn_idx) {
+  return exec_rdma_CAS(size, same_idx, same_idx, compare_add, swap, conn_idx);
 }
 
-void RdmaOps::read_mnist_update(std::vector<torch::Tensor> &update, float *local_w, int same_idx, int conn_data_idx) {
+void RdmaOps::read_mnist_update(std::vector<torch::Tensor> &update, float *local_w, int same_idx, int conn_idx) {
   // Read the update from the server
   Logger::instance().log("Client: Read weights from server DFA\n");
-  if (conn_data_idx != 0) {
-    exec_rdma_read(REG_SZ_DATA, same_idx, conn_data_idx);
+  if (conn_idx != 0) {
+    exec_rdma_read(REG_SZ_DATA, same_idx, conn_idx);
   } else {
     exec_rdma_read(REG_SZ_DATA, same_idx);
   }
