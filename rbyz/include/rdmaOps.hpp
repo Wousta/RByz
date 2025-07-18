34137a7c1b15b61042cd5d28902d85017f4386f9
Wousta <luisbmi55@gmail.com>
2025-07-18 08:55:14 +0200

    Refactor RDMA operations to use enum for sync mode in exec_rdma_read and exec_rdma_write methods

--- rbyz/include/rdmaOps.hpp
+++ rbyz/include/rdmaOps.hpp
@@ -16,17 +16,18 @@ class RdmaOps {
   NetFlags netflags_no_sync;
 
   public:
+  enum Mode {sync, async};
   RdmaOps(std::vector<RcConn>& conns);
   ~RdmaOps();
 
   int exec_rdma_read(uint32_t size, uint32_t loc_info_idx, uint32_t rem_info_idx, int conn_idx = 0);
   int exec_rdma_read(uint32_t size, uint32_t same_idx, int conn_idx = 0);
-  int exec_rdma_read(uint32_t size, LocalInfo &local_info, RemoteInfo &remote_info, int conn_idx = 0, bool is_sync = false);
-  
+  int exec_rdma_read(uint32_t size, LocalInfo &local_info, RemoteInfo &remote_info, int conn_idx = 0, Mode mode = Mode::sync);
+
   int exec_rdma_write(uint32_t size, uint32_t loc_info_idx, uint32_t rem_info_idx, int conn_idx = 0);
   int exec_rdma_write(uint32_t size, uint32_t same_idx, int conn_idx = 0);
-  int exec_rdma_write(uint32_t size, LocalInfo &local_info, RemoteInfo &remote_info, int conn_idx = 0, bool is_sync = false);
-  
+  int exec_rdma_write(uint32_t size, LocalInfo &local_info, RemoteInfo &remote_info, int conn_idx = 0, Mode mode = Mode::sync);
+
   int exec_rdma_CAS(uint32_t size, uint32_t loc_info_idx, uint32_t rem_info_idx, uint64_t compare_add, uint64_t swap, int conn_idx = 0);
   int exec_rdma_CAS(uint32_t size, uint32_t same_idx, uint64_t compare_add, uint64_t swap, int conn_idx = 0);
 };
